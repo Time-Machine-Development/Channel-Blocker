94bda44e0e8774b73bd20112e1a32dc105c67971
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2018-04-15 16:55:37 +0200

    modified generic ids for add-buttons, delete-buttons and selections,\n implemented and tested event_dispatcher.js\n and did some minor fixes

---
+++ config/js/event_dispatcher.js
@@ -0,0 +1,56 @@
+{
+	const SENDER = "config_event_dispatcher";
+	const CONTAINER_IDS = [
+		"blocked_users",
+		"title_regexs",
+		"name_regexs",
+		"comment_regexs",
+		"excluded_users"
+	];
+
+	function sendMessage(type, origin, input){
+		browser.runtime.sendMessage(
+			{
+				sender: SENDER,
+				"event": {
+					type: 	type,
+					origin: origin,
+					input: 	input
+				}
+			}
+		);
+	}
+
+	function sendAddMessage(containerId){
+		let input = document.getElementById("input_textfield").value;
+
+		sendMessage("add", containerId, input);
+	}
+
+	function sendDeleteMessage(containerId){
+		function getSelectedOptions(selectionId){
+			let selection = document.getElementById(selectionId);
+			let options = [];
+
+			for(let opt of selection.selectedOptions){
+				options.push(opt.getAttribute("value"));
+			}
+
+			return options;
+		}
+
+		let selectionId = containerId + "_selection";
+		let input = getSelectedOptions(selectionId);
+
+		sendMessage("delete", containerId, input);
+	}
+
+	//install onclick functions for all buttons of config.html
+	for(let containerId of CONTAINER_IDS){
+		let addBtnId = containerId + "_add_btn";
+		let deleteBtnId = containerId + "_delete_btn";
+
+		document.getElementById(addBtnId).onclick = () => {sendAddMessage(containerId)};
+		document.getElementById(deleteBtnId).onclick = () => {sendDeleteMessage(containerId)};
+	}
+}
--- config/js/user_interaction.js
+++ config/js/user_interaction.js
@@ -1,14 +1,16 @@
-function checkboxHandler() {
-	let showAdvancedView = document.getElementById("configurationCheckbox").checked;
-	if(showAdvancedView === true){
-		document.getElementById("hideable").style.display = "block";
-	}else{
-		document.getElementById("hideable").style.display = "none";
+{
+	function checkboxHandler() {
+		let showAdvancedView = document.getElementById("configurationCheckbox").checked;
+		if(showAdvancedView === true){
+			document.getElementById("hideable").style.display = "block";
+		}else{
+			document.getElementById("hideable").style.display = "none";
+		}
 	}
+
+	//handle initial checkbox status
+	checkboxHandler();
+
+	//define behavior for clicking the checkbox
+	document.getElementById("configurationCheckbox").onclick = checkboxHandler;
 }
-
-//check initial checkbox status
-checkboxHandler();
-
-//define behaviour for clicking the checkbox
-document.getElementById("configurationCheckbox").onclick = checkboxHandler;
