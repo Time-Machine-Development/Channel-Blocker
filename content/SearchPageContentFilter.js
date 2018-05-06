87366c949789e93433d48c02e983a5f2d75139fc
Marc Beyer <marc.beyer.dev@gmail.com>
2018-05-06 11:58:10 +0200

    fixed bugs at StartPageContent

--- content/SearchPageContentFilter.js
+++ content/SearchPageContentFilter.js
@@ -2,47 +2,41 @@
 	
 	function SearchPageContentFilter(target, parent) {
 		this.onFound = function(child){
+			
 			if(child.tagName === "YTD-SHELF-RENDERER"){
-				for(elem of document.getElementsByClassName("style-scope ytd-vertical-list-renderer")){
+				for(elem of child.getElementsByClassName("style-scope ytd-vertical-list-renderer")){
 					if(elem.id == "items"){
+						console.log("<"+child.tagName+" "+child.className+">");
 						console.log("new SearchPageContentFilter");
-						try{
-							var filter2 = new SearchPageContentFilter(elem, this);
-						}catch(e){
-							console.log(e);
-						}
+						var filter = new SearchPageContentFilter(elem, this);
 					}
 				}
 			}
 			
 			if(child.tagName === "YTD-VIDEO-RENDERER"){
-				try{
-					let linkInnerArr = child.getElementsByTagName("a");
-					if(linkInnerArr.length >= 3){
-						console.log("Video: '" + linkInnerArr[1].textContent + "' from " + linkInnerArr[2].textContent);
-						
-						if(checkVideoTilte(linkInnerArr[1].textContent)){
-							child.remove();
-						}
-						if(checkUserChannelName(linkInnerArr[2].textContent)){
-							child.remove();
-						}
-						
-						if(debug){
-							linkInnerArr[1].style.color = "darkgray";
-							linkInnerArr[2].style.color = "darkgray";
-						}
-						
-						for(let btnContainerElem of child.getElementsByClassName("text-wrapper style-scope ytd-video-renderer")){
-							btnContainerElem.appendChild(createBtnNode(linkInnerArr[2].textContent));
-						}
+				let linkInnerArr = child.getElementsByTagName("a");
+				if(linkInnerArr.length >= 3){
+					console.log("Video: '" + linkInnerArr[1].textContent + "' from " + linkInnerArr[2].textContent);
+					
+					if(checkVideoTilte(linkInnerArr[1].textContent)){
+						child.remove();
+					}
+					if(checkUserChannelName(linkInnerArr[2].textContent)){
+						child.remove();
 					}
 					
 					if(debug){
-						child.style.background = "green";
+						linkInnerArr[1].style.color = "darkgray";
+						linkInnerArr[2].style.color = "darkgray";
 					}
-				}catch(e){
-					console.log(e);
+					
+					for(let btnContainerElem of child.getElementsByClassName("text-wrapper style-scope ytd-video-renderer")){
+						btnContainerElem.appendChild(createBtnNode(linkInnerArr[2].textContent));
+					}
+				}
+				
+				if(debug){
+					child.style.background = "green";
 				}
 			}
 			
