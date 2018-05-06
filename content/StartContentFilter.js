a662a05692996d10cc3dab8a10776feb778e9a6b
Marc Beyer <marc.beyer.dev@gmail.com>
2018-05-06 07:09:33 +0200

    added Filter, StartContentFilter and VideoContentFilter

---
+++ content/StartContentFilter.js
@@ -0,0 +1,65 @@
+
+
+	function StartContentFilter(target, parent) {
+		this.onFound = function(child){
+			
+			//OuterVideo-container
+			for(let elem of child.getElementsByClassName("style-scope ytd-shelf-renderer")){
+				if(elem.id == "title"){
+					console.log("Container: " + elem.textContent);
+				}
+			}
+			//Empfohlen-container
+			for(let elem of child.getElementsByClassName("style-scope yt-horizontal-list-renderer")){
+				if(elem.id == "items"){
+					var filter2 = new VideoContainerFilter(elem, this);
+				}
+			}
+			//SingleVideo-container
+			for(let elem of child.getElementsByClassName("style-scope ytd-expanded-shelf-contents-renderer")){
+				if(elem.id == "grid-container"){
+					for(let inElem of elem.getElementsByClassName("yt-simple-endpoint style-scope ytd-video-renderer")){
+						if(inElem.id == "video-title"){
+							console.log("Video: " + inElem.getAttribute("title"));
+						}
+					}
+					
+					if(debug){
+						elem.style.background = "green";
+					}
+				}
+			}
+			//gridVideo/chanel-container
+			for(let elem of child.getElementsByClassName("style-scope ytd-grid-renderer")){
+				if(elem.id == "items"){
+					var filter2 = new VideoContainerFilter(elem, this);
+				}
+			}
+		}
+		
+		Filter.call(this, target, parent);
+	}
+
+	StartContentFilter.prototype = Object.create(Filter.prototype);
+
+	StartContentFilter.prototype.constructor = StartContentFilter;
+
+	StartContentFilter.prototype.onFoundInit = function(child){
+		this.onFound(child);
+		
+		if(debug){
+			child.style.background = "red";
+		}
+	};
+	
+	StartContentFilter.prototype.onFoundObs = function(child){
+		this.onFound(child);
+		
+		if(debug){
+			child.style.background = "orange";
+		}
+	};
+	
+	
+	var filter1 = new StartContentFilter(document.getElementsByTagName("ytd-item-section-renderer")[0].parentNode);
+	console.log(filter1.childFilters);
---
+++ content/VideoContainerFilter.js
@@ -0,0 +1,36 @@
+
+	function VideoContainerFilter(target, parent) {
+		this.onFound = function(child){
+			let linkInnerArr = child.getElementsByTagName("a");
+			if(linkInnerArr.length >= 3){
+				console.log("GridVideo: '" + linkInnerArr[1].textContent + "' from " + linkInnerArr[2].textContent);
+				
+				if(debug){
+					linkInnerArr[1].style.color = "darkgray";
+					linkInnerArr[2].style.color = "darkgray";
+				}
+			}
+		}
+		
+		Filter.call(this, target, parent);
+	}
+
+	VideoContainerFilter.prototype = Object.create(Filter.prototype);
+
+	VideoContainerFilter.prototype.constructor = VideoContainerFilter;
+
+	VideoContainerFilter.prototype.onFoundInit = function(child){
+		this.onFound(child);
+		
+		if(debug){
+			child.style.background = "green";
+		}
+	};
+	
+	VideoContainerFilter.prototype.onFoundObs = function(child){
+		this.onFound(child);
+		
+		if(debug){
+			child.style.background = "blue";
+		}
+	};
