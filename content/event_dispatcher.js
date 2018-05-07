f3ba1da7295338c1ea9fd49f95fbefdd165dd701
Marc Beyer <marc.beyer.dev@gmail.com>
2018-05-07 21:33:11 +0200

    change msg-objects

--- content/event_dispatcher.js
+++ content/event_dispatcher.js
@@ -2,7 +2,16 @@
 	function blockUserChannel (userChannelName){
 		console.log(userChannelName + " blocked!");
 		alert(userChannelName + " blocked!");
-		let sending = browser.runtime.sendMessage({toBlock:userChannelName});
+		let msg = {
+			sender: "background_controller_storage",
+			receiver: "content_event_dispatcher",
+			"event": {
+				type: "add",
+				origin: "blocked_users",
+				input: userChannelName
+			}
+		};
+		let sending = browser.runtime.sendMessage(msg);
 	}
 	
 
