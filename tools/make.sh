96a303cc727506ca2231c8e78d77f381085d810d
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2018-05-18 01:35:51 +0200

    modified make.sh according to the new folder structure; added folder bin to gitignore which will be used for the builded zip

--- tools/make.sh
+++ tools/make.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #lazy creating all directories
-mkdir $1/build
-mkdir $1/build/content
-mkdir $1/build/config
-mkdir $1/build/config/js
-mkdir $1/build/background
-mkdir $1/build/shared
+mkdir ./build
+mkdir ./build/content
+mkdir ./build/config
+mkdir ./build/config/js
+mkdir ./build/background
+mkdir ./build/shared
 
 #writing all files without comments from $1 (reference to source folder) into ./build
-for i in $(find $1 | grep "\.js$" | grep -v "build"); do
-	j=$1/build/$(echo $i| cut -c 3-)
+for i in $(find ./src | grep "\.js$" | grep -v "build"); do
+	j=./build/$(echo $i| cut -c 7-)
 	pcregrep -h -v -e "^[^\"]*//(.)*$" $i | pcregrep -h -v -M -e "^[^\"]*/\*(.|\n)*\*/$" > $j
 done
 
-#TODO create zip from ./build
+#TODO create zip from ./build and move result to ./bin
