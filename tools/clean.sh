bf2fe7704de9f0397368fe148ca6207a8c00b412
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2018-05-22 19:05:38 +0200

    finished implementation of make.sh and clean.sh; make.sh will now also zip all build files together into a ytc.xpi file inside a bin folder; clean.sh will recursivly remove the folder build and bin

---
+++ tools/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#has to be executed from inside folder ytc
+
+rm -r ./build
+rm -r ./bin
--- tools/make.sh
+++ tools/make.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#lazy creating all directories
+#has to be executed from inside folder ytc
+
+#lazy creating all directories for build files
 mkdir ./build
 mkdir ./build/content
 mkdir ./build/config
@@ -8,10 +10,19 @@ mkdir ./build/config/js
 mkdir ./build/background
 mkdir ./build/shared
 
-#writing all files without comments from $1 (reference to source folder) into ./build
+#writing all files without comments from ./src (reference to source folder) into ./build
 for i in $(find ./src | grep "\.js$" | grep -v "build"); do
 	j=./build/$(echo $i| cut -c 7-)
 	pcregrep -h -v -e "^[^\"]*//(.)*$" $i | pcregrep -h -v -M -e "^[^\"]*/\*(.|\n)*\*/$" > $j
 done
 
-#TODO create zip from ./build and move result to ./bin
+#copy manifest.json to ./build
+cp ./src/manifest.json ./build/manifest.json
+
+#create directory for final project file
+mkdir ./bin
+
+#create zip from build inside bin folder
+cd build
+zip -r -FS ../bin/ytc.xpi *
+cd ..
