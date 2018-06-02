286218ab2d2544f8ea5956f50ac3993463f85cb1
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2018-06-02 17:48:25 +0200

    deleted unneccesarry class VideoContainerHeaderFilter; renamed class VideoContentFilter to StartContentFilter; moved defintion of methods from constructor into global space (with prototype); removed onFoundInit- and onFoundObs-methods

---
+++ src/content/StartContainerFilter.js
@@ -0,0 +1,49 @@
+function StartContainerFilter(target, parent) {
+	Filter.call(this, target, parent);
+}
+
+StartContainerFilter.prototype = Object.create(Filter.prototype);
+
+StartContainerFilter.prototype.constructor = StartContainerFilter;
+
+StartContainerFilter.prototype.onFound = function(child){
+
+	let userChannelName;
+
+	let linkInnerArr = child.getElementsByTagName("a");
+	if(linkInnerArr.length >= 3){
+		let cFilter = new CallbackFilter(linkInnerArr[2], this);
+		userChannelName = linkInnerArr[2].textContent;
+
+		//GridVideo-Container
+		checkVideoTitle(userChannelName, linkInnerArr[1].textContent, child);
+
+		if(debug){
+			linkInnerArr[1].style.color = "darkgray";
+			linkInnerArr[2].style.color = "darkgray";
+		}
+	}else{
+
+		//Channel-Container
+		for(let elem of child.getElementsByClassName("style-scope ytd-grid-channel-renderer")){
+			if(elem.id == "title"){
+				userChannelName = elem.textContent;
+				checkUserChannelName(userChannelName, elem.textContent, child);
+			}
+		}
+	}
+
+	//insert button to block channel/user
+	for(let btnContainerElem of child.getElementsByClassName("style-scope ytd-grid-video-renderer")){
+		if(btnContainerElem.id === "byline-container"){
+			createBtnAtStart(btnContainerElem, createBtnNode(userChannelName));
+		}
+	}
+};
+
+StartContainerFilter.prototype.reload = function(){
+	console.log("--RELOAD--");
+	for(let childElement of this.target.children){
+		this.onFound(childElement);
+	}
+};
--- src/content/StartContentFilter.js
+++ src/content/StartContentFilter.js
@@ -1,77 +1,4 @@
 function StartContentFilter(target, parent) {
-	this.onFound = function(child){
-		
-		//OuterVideo-container
-		for(let elem of child.getElementsByClassName("style-scope ytd-shelf-renderer")){
-			if(elem.id == "title"){
-				let cFilter = new CallbackFilter(elem, this);
-				checkUserChannelName(elem.textContent, child);
-				
-				//insert button to block channel/user
-				console.log("TAGNAME: " + elem.parentNode.tagName);
-				if(elem.textContent !== ""){
-					if(elem.parentNode.tagName !== "H2"){
-						createBtnAfter(elem.parentNode.parentNode, createContainerBtnNode(elem.textContent));
-					}else{
-						createBtnAfter(elem.parentNode, createContainerBtnNode(elem.textContent));
-					}
-				}
-			}
-		}
-		
-		//Empfohlen-container
-		for(let elem of child.getElementsByClassName("style-scope yt-horizontal-list-renderer")){
-			if(elem.id == "items"){
-				var filter2 = new VideoContainerFilter(elem, this);
-			}
-		}
-		
-		//SingleVideo-container
-		for(let elem of child.getElementsByClassName("style-scope ytd-expanded-shelf-contents-renderer")){
-			if(elem.id == "grid-container"){
-				for(videoElem of elem.children){
-					let linkInnerArr = videoElem.getElementsByTagName("a");
-					if(linkInnerArr.length >= 3){
-						let cFilter = new CallbackFilter(linkInnerArr[2].parentNode, this);
-						
-						checkVideoTitle(linkInnerArr[2].textContent, linkInnerArr[1].textContent, videoElem);
-						
-						//insert button to block channel/user
-						for(let btnContainerElem of videoElem.getElementsByClassName("style-scope ytd-video-meta-block")){
-							if(btnContainerElem.id === "metadata" && linkInnerArr[2].textContent != ""){
-								
-								createBtnAtStart(btnContainerElem, createBtnNode(linkInnerArr[2].textContent));
-							}
-						}
-						
-						if(debug){
-							linkInnerArr[1].style.color = "darkgray";
-							linkInnerArr[2].style.color = "darkgray";
-						}
-					}
-					
-					if(debug){
-						videoElem.style.background = "green";
-					}
-				}
-			}
-		}
-		
-		//gridVideo/channel-container
-		for(let elem of child.getElementsByClassName("style-scope ytd-grid-renderer")){
-			if(elem.id == "items"){
-				var filter2 = new VideoContainerFilter(elem, this);
-			}
-		}
-	}
-	
-	this.reload = function(){
-		console.log("--RELOAD--");
-		for(let childElement of this.target.children){
-			this.onFound(childElement);
-		}
-	}
-	
 	Filter.call(this, target, parent);
 }
 
@@ -79,18 +6,75 @@ StartContentFilter.prototype = Object.create(Filter.prototype);
 
 StartContentFilter.prototype.constructor = StartContentFilter;
 
-StartContentFilter.prototype.onFoundInit = function(child){
-	this.onFound(child);
-	
-	if(debug){
-		child.style.background = "red";
+StartContentFilter.prototype.onFound = function(child){
+
+	//OuterVideo-container
+	for(let elem of child.getElementsByClassName("style-scope ytd-shelf-renderer")){
+		if(elem.id == "title"){
+			let cFilter = new CallbackFilter(elem, this);
+			checkUserChannelName(elem.textContent, child);
+
+			//insert button to block channel/user
+			console.log("TAGNAME: " + elem.parentNode.tagName);
+			if(elem.textContent !== ""){
+				if(elem.parentNode.tagName !== "H2"){
+					createBtnAfter(elem.parentNode.parentNode, createContainerBtnNode(elem.textContent));
+				}else{
+					createBtnAfter(elem.parentNode, createContainerBtnNode(elem.textContent));
+				}
+			}
+		}
+	}
+
+	//Empfohlen-container
+	for(let elem of child.getElementsByClassName("style-scope yt-horizontal-list-renderer")){
+		if(elem.id == "items"){
+			var filter2 = new VideoContainerFilter(elem, this);
+		}
+	}
+
+	//SingleVideo-container
+	for(let elem of child.getElementsByClassName("style-scope ytd-expanded-shelf-contents-renderer")){
+		if(elem.id == "grid-container"){
+			for(videoElem of elem.children){
+				let linkInnerArr = videoElem.getElementsByTagName("a");
+				if(linkInnerArr.length >= 3){
+					let cFilter = new CallbackFilter(linkInnerArr[2].parentNode, this);
+
+					checkVideoTitle(linkInnerArr[2].textContent, linkInnerArr[1].textContent, videoElem);
+
+					//insert button to block channel/user
+					for(let btnContainerElem of videoElem.getElementsByClassName("style-scope ytd-video-meta-block")){
+						if(btnContainerElem.id === "metadata" && linkInnerArr[2].textContent != ""){
+
+							createBtnAtStart(btnContainerElem, createBtnNode(linkInnerArr[2].textContent));
+						}
+					}
+
+					if(debug){
+						linkInnerArr[1].style.color = "darkgray";
+						linkInnerArr[2].style.color = "darkgray";
+					}
+				}
+
+				if(debug){
+					videoElem.style.background = "green";
+				}
+			}
+		}
+	}
+
+	//gridVideo/channel-container
+	for(let elem of child.getElementsByClassName("style-scope ytd-grid-renderer")){
+		if(elem.id == "items"){
+			var filter2 = new VideoContainerFilter(elem, this);
+		}
 	}
 };
 
-StartContentFilter.prototype.onFoundObs = function(child){
-	this.onFound(child);
-	
-	if(debug){
-		child.style.background = "orange";
+StartContentFilter.prototype.reload = function(){
+	console.log("--RELOAD--");
+	for(let childElement of this.target.children){
+		this.onFound(childElement);
 	}
 };
