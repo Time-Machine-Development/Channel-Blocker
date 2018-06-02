378202d78fcb9c114bba5411cbdfc7ada6e13f1c
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2018-06-02 12:22:21 +0200

    removed onFoundInit and onFoundObs from class Filter and replaced them by onFound

--- src/content/Filter.js
+++ src/content/Filter.js
@@ -1,29 +1,25 @@
+//DELETE THIS
 var debug = false;
 
 function Filter(target, parent) {
 	this.mutationObs = new MutationObserver(this.callbackFunc);
 	this.mutationObs.filterInst = this;
 	this.mutationObs.observe(target, {childList: true});
-	
+
 	this.target = target;
-	
+
 	this.childFilters = new Array();
-	
+
 	for(let childElement of target.children){
 		this.onFoundInit(childElement);
 	}
-	
+
 	if(parent !== undefined){
 		parent.childFilters.push(this);
 	}
 }
 
-Filter.prototype.onFoundInit = function(child){
-	console.log(child);
-};
-
-Filter.prototype.onFoundObs = function(child){
-	console.log(child);
+Filter.prototype.onFound = function(child){
 };
 
 Filter.prototype.callbackFunc = function(mutationRecArr, observerInst){
