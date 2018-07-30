3d817d1fd9f96088fb4e20c11820d916bed6def6
Marc Beyer <marc.beyer.dev@gmail.com>
2018-07-30 22:46:01 +0200

    added darkTheme

--- src/config/js/user_interaction.js
+++ src/config/js/user_interaction.js
@@ -8,9 +8,22 @@
 		}
 	}
 
+	function darkThemeCheckboxHandler() {
+		let showDarkTheme = document.getElementById("darkThemeCheckbox").checked;
+		if(showDarkTheme === true){
+			document.getElementById("css").href = "styleDark.css";
+		}else{
+			document.getElementById("css").href = "style.css";
+		}
+	}
+	
 	//handle initial checkbox status
 	checkboxHandler();
+	darkThemeCheckboxHandler();
 
 	//define behavior for clicking the checkbox
 	document.getElementById("configurationCheckbox").onclick = checkboxHandler;
+	
+	//define behavior for clicking the checkbox
+	document.getElementById("darkThemeCheckbox").onclick = darkThemeCheckboxHandler;
 }
