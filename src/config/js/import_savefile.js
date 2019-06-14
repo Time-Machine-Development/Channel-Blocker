0ac0d98f03bcabb706309ffea7f09186f11a5061
Marc Beyer <marc.beyer.dev@gmail.com>
2019-06-14 07:36:13 +0200

    added legacy import

--- src/config/js/import_savefile.js
+++ src/config/js/import_savefile.js
@@ -4,7 +4,7 @@
 	/* creates an "add"-message for background_filter_storage
 	if regExType is undefined (e.g. not passed) userChannelNameOrRegEx is an user/channel-name,
 	otherwise it is a regular expression with type regExType */
-	function createAddMsg(filterType, userChannelNameOrRegEx, regExType){
+	function createAddMsg(filterType, userChannelNameOrRegEx, regExType) {
 		let msg = {
 			sender: SENDER,
 			receiver: "background_filter_storage",
@@ -14,9 +14,9 @@
 			}
 		};
 
-		if(regExType === undefined){
+		if (regExType === undefined) {
 			msg.content.user_channel_name = userChannelNameOrRegEx;
-		}else{
+		} else {
 			msg.content.reg_exp = userChannelNameOrRegEx;
 			msg.content.reg_exp_type = regExType;
 		}
@@ -25,22 +25,22 @@
 	}
 
 	//creates a "config_value_set"-message for background_config_storage
-     function createConfigValueSetMsg(configId, configVal){
-          return {
-               sender: SENDER,
-               receiver: "background_config_storage",
-               content: {
-                    info: "config_value_set",
-                    config_id: configId,
-                    config_val: configVal
-               }
-          };
-     }
-	
+	function createConfigValueSetMsg(configId, configVal) {
+		return {
+			sender: SENDER,
+			receiver: "background_config_storage",
+			content: {
+				info: "config_value_set",
+				config_id: configId,
+				config_val: configVal
+			}
+		};
+	}
+
 	//Get the choosen file and read it as text
 	function startRead(event) {
 		let file = document.getElementById('fileLoaderBtn').files[0];
-		if(file){
+		if (file) {
 			let fileReader = new FileReader();
 
 			fileReader.readAsText(file, "UTF-8");
@@ -54,22 +54,37 @@
 		let fileString = event.target.result;
 		let jsonSaveFile = JSON.parse(fileString);
 
-		for(let key in jsonSaveFile){
-			if(key === "config"){
-				for(let elem of Object.keys(jsonSaveFile[key])){
-					browser.runtime.sendMessage(createConfigValueSetMsg(elem, jsonSaveFile[key][elem]));
+		if (Object.keys(jsonSaveFile).includes('config')) {
+			//New savefile-format
+			for (let key in jsonSaveFile) {
+				if (key === "config") {
+					for (let elem of Object.keys(jsonSaveFile[key])) {
+						browser.runtime.sendMessage(createConfigValueSetMsg(elem, jsonSaveFile[key][elem]));
+					}
+				} else {
+					for (let elem of Object.keys(jsonSaveFile[key])) {
+						browser.runtime.sendMessage(createAddMsg(key, elem, jsonSaveFile[key][elem]));
+					}
 				}
-			}else{
-				for(let elem of Object.keys(jsonSaveFile[key])){
-					browser.runtime.sendMessage(createAddMsg(key, elem, jsonSaveFile[key][elem]));
+			}
+		} else {
+			//Old savefile-format
+			for (let key in jsonSaveFile) {
+				if (key == "5") {
+					//config
+				}else {
+					for (let elem of jsonSaveFile[key]) {
+						browser.runtime.sendMessage(createAddMsg(key, elem, 0));
+					}
 				}
 			}
 		}
+
 	}
 
 	//Check if the file-APIs are supported.
 	if (window.File && window.FileReader && window.FileList && window.Blob) {
-	  	//The file-APIs are supported.
+		//The file-APIs are supported.
 		document.getElementById('fileLoaderBtn').addEventListener('change', startRead, false);
 	} else {
 		//The file-APIs are supported.
