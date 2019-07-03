de91cd02b0e7a91c4be3dbd0d16ac3d23f59b9ee
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2019-07-03 21:17:21 +0200

    modified comment and implementation of the visual representation of the case-sensesitivity on config.html

--- src/config/js/filter_user_interaction.js
+++ src/config/js/filter_user_interaction.js
@@ -69,9 +69,13 @@
 		option.setAttribute("value", userChannelNameOrRegEx);
 		option.textContent = "\"" + userChannelNameOrRegEx + "\"";
 
-		//regExType should only be not undefined if and only if containerId is in {TITLE_REGEXS, NAME_REGEXS, COMMENT_REGEXS}
-		if(regExType === RegExType.CASE_INSENSITIVE){
-			option.textContent += "*";
+		/*regExType should only be interpreted if and only if containerId is (FilterType.TITLE_REGEXS | FilterType.NAME_REGEXS | FilterType.COMMENT_REGEXS)
+		because in older versions no concept of a regular expression type existed, nevertheless the part of the storage
+		which is now interpreted as the regular expression was filled with certain values e.g. 53, true and potentially other*/
+		if(containerId === FilterType.TITLE_REGEXS || containerId === FilterType.NAME_REGEXS || containerId === FilterType.COMMENT_REGEXS){
+			if(regExType === RegExType.CASE_INSENSITIVE){
+				option.textContent += "*";
+			}
 		}
 	}
 
