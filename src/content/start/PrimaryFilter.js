3bd947c80d8b62d762fb53d74577586d88acd06f
Marc Beyer <marc.beyer.dev@gmail.com>
2019-08-31 12:22:34 +0200

    changed button layout on videopage

--- src/content/start/PrimaryFilter.js
+++ src/content/start/PrimaryFilter.js
@@ -8,9 +8,7 @@ PrimaryFilter.prototype = Object.create(Filter.prototype);
 PrimaryFilter.prototype.constructor = PrimaryFilter;
 
 PrimaryFilter.prototype.onFound = function(child){
-	console.log("PrimaryFilter child-found",child);
 	for(elem of child.children){
-		console.log("child-child",elem);
 		if(elem.id === "contents"){
 			new StartContentFilter(elem, this);
 		}
