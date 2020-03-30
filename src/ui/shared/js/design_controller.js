a8a3e2d9a7cfea7a218276f238e56ac2e2360f11
Marc Beyer <marc.beyer.dev@gmail.com>
2020-03-30 19:48:12 +0200

    design changes for bug, faq and popup are now possible

---
+++ src/ui/shared/js/design_controller.js
@@ -0,0 +1,33 @@
+{
+    const SENDER = "ui_shared_design_controller";
+
+	//creates a "config_value_request"-message for background_config_storage
+	function createConfigValueRequestMsg(configId) {
+		return {
+			sender: SENDER,
+			receiver: "background_config_storage",
+			content: {
+				info: "config_value_request",
+				config_id: configId
+			}
+		};
+	}
+    
+    //request the disign of this page
+    async function requestDesignStyle(){
+        let val = await browser.runtime.sendMessage(createConfigValueRequestMsg(ConfigId.CONFIG_PAGE_DESIGN));
+        changePageDesign(val);
+    }
+    
+    //change the css-style of the config page
+    function changePageDesign(configValue) {
+        if (configValue === "0") {
+            document.getElementById("css").href = "../../shared/css/dark_root.css";
+        } else if (configValue === "1") {
+            document.getElementById("css").href = "../../shared/css/light_root.css";
+        }
+    }
+
+    //start a request for the design of the page
+    requestDesignStyle();
+}
