a39d727de551aa0922888335bebb9ea241e41e8c
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2020-04-05 14:51:31 +0200

    minimized checker_module.js and event_dispatcher.js

--- src/content/event_dispatcher.js
+++ src/content/event_dispatcher.js
@@ -14,14 +14,3 @@ function createBlockUserChannelFunction(userChannelName){
 		browser.runtime.sendMessage(createAddBlockedUserMsg(userChannelName));
 	};
 }
-
-//?: what means 'still correct', what is the loop doing
-//Checks if the 'userChannelName' is still correct and invokes 'blockUserChannel()'
-function blockUserChannelwithFallback(userChannelName, elem){
-	for(let item of elem.parentNode.getElementsByClassName("yt-simple-endpoint style-scope yt-formatted-string")){
-		userChannelName = item.textContent;
-	}
-
-	//sends an "add_blocked_user"-message to the background_filter_storage which adds userChannelName to the set of blocked users
-	blockUserChannel(userChannelName);
-}
