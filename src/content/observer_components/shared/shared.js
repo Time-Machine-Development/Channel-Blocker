f0a4cd8dfefc826b1afe0d1204c82036ec4482c9
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2020-04-22 19:37:38 +0200

    modified trending-observer-components

--- src/content/observer_components/shared/shared.js
+++ src/content/observer_components/shared/shared.js
@@ -1,5 +1,11 @@
-async function onVideoObserved(video, characterDatas, characterDataParents){
+async function onVerticalVideoObserved(video, characterDatas, characterDataParents){
 	insertBlockBtnBefore(characterDataParents.userChannelName, characterDatas.userChannelName);
 
-	toggleVisibilty(video, await isVideoTitleBlocked(characterDatas.userChannelName, characterDatas.videoTitle));
+	toggleVisibiltyHorizontal(video, await isVideoTitleBlocked(characterDatas.userChannelName, characterDatas.videoTitle));
+}
+
+async function onHorizontalVideoObserved(video, characterDatas, characterDataParents){
+	insertBlockBtnBefore(characterDataParents.userChannelName, characterDatas.userChannelName);
+
+	toggleVisibiltyVertical(video, await isVideoTitleBlocked(characterDatas.userChannelName, characterDatas.videoTitle));
 }
