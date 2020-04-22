29344d4359baccea5acbe88ba70459d432a9842d
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2020-04-22 19:54:26 +0200

    modified home-observer-components

--- src/content/observer_components/shared/shared.js
+++ src/content/observer_components/shared/shared.js
@@ -1,3 +1,9 @@
+async function onVideoObserved(video, characterDatas, characterDataParents){
+	insertBlockBtnBefore(characterDataParents.userChannelName, characterDatas.userChannelName);
+
+	toggleVisibilty(video, await isVideoTitleBlocked(characterDatas.userChannelName, characterDatas.videoTitle));
+}
+
 async function onVerticalVideoObserved(video, characterDatas, characterDataParents){
 	insertBlockBtnBefore(characterDataParents.userChannelName, characterDatas.userChannelName);
 
