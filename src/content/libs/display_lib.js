d7db5ac0ab8ae7eab84911d7919214477718e8f9
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2020-04-23 13:55:21 +0200

    fixed bug

--- src/content/libs/display_lib.js
+++ src/content/libs/display_lib.js
@@ -15,17 +15,14 @@ function fade(element, isBlocked){
 }
 
 function toggleVisibiltyVertical(element, isBlocked){
-	console.log("vertical");
-
 	if(isBlocked){
-		$(element).fadeOut(contentConfig[ConfigId.CONTENT_ANIMATION_SPEED]);
+		$(element).animate({width: "hide"}, contentConfig[ConfigId.CONTENT_ANIMATION_SPEED]);
 	}else{
 		$(element).animate({width: "show"}, contentConfig[ConfigId.CONTENT_ANIMATION_SPEED]);
 	}
 }
 
 function toggleVisibiltyHorizontal(element, isBlocked){
-	console.log("hirizontal");
 	if(isBlocked){
 		$(element).animate({height: "hide"}, contentConfig[ConfigId.CONTENT_ANIMATION_SPEED]);
 	}else{
