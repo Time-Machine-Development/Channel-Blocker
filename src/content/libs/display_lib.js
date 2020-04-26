aafe5e3db5f85957fb2ac49fbc312af857faa945
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2020-04-26 12:58:57 +0200

    added detailed comments

--- src/content/libs/display_lib.js
+++ src/content/libs/display_lib.js
@@ -15,17 +15,23 @@ function fade(element, isBlocked){
 }
 
 function toggleVisibiltyVertical(element, isBlocked){
+	//return if and only if either element is hidden and should be blocked or element is not hidden and should not be blocked
+	if((element.style.display === "none") === isBlocked){
+		return;
+	}
+
+	/* Youtube may enforce a width-height-ratio of elements s.t. for example the height of a thumbnail whose width is decreased during the animation
+	will also be decreased to match the width-height-ratio. */
 	if(isBlocked){
-		if(element.style.display !== "none"){
-			// set the style of children
-			for (let child of element.children) {
-				child.style.width = child.clientWidth + "px";
-			}
-			$(element).animate({width: "hide"}, contentConfig[ConfigId.CONTENT_ANIMATION_SPEED]);
+		//To "disable" a forced width-height-ratio the style.width of element is overwritten and thereby fixed.
+		for (let child of element.children) {
+			child.style.width = child.clientWidth + "px";
 		}
+
+		$(element).animate({width: "hide"}, contentConfig[ConfigId.CONTENT_ANIMATION_SPEED]);
 	}else if(element.style.display === "none"){
-		$(element).animate({width: "show"}, contentConfig[ConfigId.CONTENT_ANIMATION_SPEED], function() {
-			// animation complete. Reset children style
+		$(element).animate({width: "show"}, contentConfig[ConfigId.CONTENT_ANIMATION_SPEED], () => {
+			//To "enable" the default behaviour (e.g. a forced width-height-ratio) the style.width is set to "" s.t. the "default" style.width is used.
 			for (let child of element.children) {
 				child.style.width = "";
 			}
