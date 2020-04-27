d231e43ce93098c18a8865bbf3b81b152472bd3d
Marc Beyer <marc.beyer.dev@gmail.com>
2020-04-27 10:45:14 +0200

    added own contextmenu

--- src/content/contextmenu_blocking.js
+++ src/content/contextmenu_blocking.js
@@ -19,7 +19,9 @@ function createContextmenuResetMsg(){
 
 //send a ContextmenuResetMsg when right clicking on no channel element
 $(document).mousedown(async function(e){
-    await browser.runtime.sendMessage(createContextmenuResetMsg());
+    if(e.which === 3){
+        await browser.runtime.sendMessage(createContextmenuResetMsg());
+    }
 });
 
 function addContextmenuListener(element, userChannelName){
@@ -36,5 +38,53 @@ function addContextmenuListener(element, userChannelName){
     //send a ContextmenuUpdateMsg when opening the contextmenu
     $(element).contextmenu(async function(e){
         await browser.runtime.sendMessage(createContextmenuUpdateMsg(userChannelName));
+    });
+}
+
+
+
+
+///////////
+// WAY 2 //
+///////////
+
+function createAddBlockedUserMsg(userChannelName){
+	return {
+		sender: "content_block_button_lib",
+		receiver: "background_filter_storage",
+		info: "add_blocked_user",
+		content: {
+			user_channel_name: userChannelName
+		}
+	};
+}
+
+function createOwnContextMenu(userChannelName, x, y){
+    let btn = document.createElement("button");
+    btn.setAttribute("class", "cb_block_button");
+    btn.setAttribute("type", "button");
+    btn.setAttribute("title", "Block '" + userChannelName + "' (Channel Blocker)");
+    btn.setAttribute("style", "position: absolute;min-width: 15em;height: 2em;z-index: 42;background: #ffffff;color: #000000;left: " + x + "px;top: " + y + "px;");
+    btn.textContent = "Block '" + userChannelName + "'";
+
+    $(btn).on("click", () => {
+		browser.runtime.sendMessage(createAddBlockedUserMsg(userChannelName));
+    });
+    
+    return btn;
+}
+
+function addContextmenuListener2(element, userChannelName){
+    //reset all listener
+    $(element).off("contextmenu");
+    $(element).off("mousedown");
+
+    //send a ContextmenuUpdateMsg when opening the contextmenu
+    $(element).contextmenu(async function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        element.appendChild(createOwnContextMenu(userChannelName, e.clientX, e.clientY));
+        console.log("element", element);
+        return false;
     });
 }
--- src/content/observer_components/shared/shared.js
+++ src/content/observer_components/shared/shared.js
@@ -1,7 +1,7 @@
 async function onVideoObserved(video, characterDatas, characterDataParents){
 	insertBlockBtnBefore(characterDataParents.userChannelName, characterDatas.userChannelName);
 
-	addContextmenuListener(video, characterDatas.userChannelName);
+	addContextmenuListener2(video, characterDatas.userChannelName);
 
 	toggleVisibilty(video, await isVideoTitleBlocked(characterDatas.userChannelName, characterDatas.videoTitle));
 }
