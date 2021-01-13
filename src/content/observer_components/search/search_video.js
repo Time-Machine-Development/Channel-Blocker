dc3a12326255827afa4633b8d06db3e892618f53
Marc Beyer <marc.beyer.dev@gmail.com>
2021-01-13 18:14:30 +0100

    Fixed userChannelName-path on search-page

--- src/content/observer_components/search/search_video.js
+++ src/content/observer_components/search/search_video.js
@@ -1,7 +1,7 @@
 const SEARCH_VIDEO_CONFIG = Object.freeze({
 	anchorSelector: ["ytd-video-renderer[class='style-scope ytd-item-section-renderer']"],
 	characterDataSelectors: {
-        videoTitle: ["a#video-title[class='yt-simple-endpoint style-scope ytd-video-renderer']"],
-        userChannelName: ["a[class='yt-simple-endpoint style-scope yt-formatted-string']"]
+		videoTitle: ["a#video-title[class='yt-simple-endpoint style-scope ytd-video-renderer']"],
+		userChannelName: ["div#channel-info[class='style-scope ytd-video-renderer']", "a[class='yt-simple-endpoint style-scope yt-formatted-string']"]
 	}
 });
