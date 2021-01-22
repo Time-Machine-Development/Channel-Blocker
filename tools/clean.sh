f9de7d3759e36138933bac29dafe8f86082a44c7
Markus Sawatzki <50970356+markussawatzki@users.noreply.github.com>
2021-01-22 22:23:05 +0100

    build scripts now work in arbitrary-named project root folder

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#print error and exit, if not executed inside folder */ytc
-if [[ $(pwd | grep -v -E "ytc$") ]]; then
-	echo "ERROR: needs to be executed in */ytc"
+#print error and exit, if not executed inside the project root directory
+if ! [[ -d "tools" ]]; then
+	echo "ERROR: needs to be executed in the project root directory"
 	exit
 fi
 
