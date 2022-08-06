1f95c9685b0cf82966aa79d35febf531ffb4974a
Marc Beyer <marc.beyer.dev@gmail.com>
2022-08-06 18:26:43 +0200

    Fixed missing block button on vertiacl search

--- src/content/observer_components/search/vertical_video.js
+++ src/content/observer_components/search/vertical_video.js
@@ -1,7 +1,22 @@
 const VERTICAL_VIDEO_CONFIG = Object.freeze({
-	anchorSelector: ["ytd-video-renderer[class='style-scope ytd-vertical-list-renderer']"],
-	characterDataSelectors: {
+    anchorSelector: ["ytd-video-renderer[class='style-scope ytd-vertical-list-renderer']"],
+    characterDataSelectors: {
         videoTitle: ["a#video-title[class='yt-simple-endpoint style-scope ytd-video-renderer']"],
-        userChannelName: ["a[class='yt-simple-endpoint style-scope yt-formatted-string']"]
-	}
+        userChannelName: ["a[class='yt-simple-endpoint style-scope yt-formatted-string']"],
+    },
 });
+
+async function onVerticalSearchVideoObserved(video, characterDatas, characterDataParents) {
+    let beforeBlockBtnList = $(video).find("a[class='yt-simple-endpoint style-scope yt-formatted-string']");
+
+    // YouTube added another channelNameATag and kept the old one hidden
+    // This might be changed in the future
+    for (const beforeBlockBtn of beforeBlockBtnList) {
+        insertBlockBtnBefore(beforeBlockBtn, characterDatas.userChannelName);
+    }
+
+    toggleVisibiltyHorizontal(
+        video,
+        await isVideoTitleBlocked(characterDatas.userChannelName, characterDatas.videoTitle),
+    );
+}
--- src/content/observers_creator.js
+++ src/content/observers_creator.js
@@ -1,67 +1,67 @@
-function createHomeObservers(){
-	let obs = [];
+function createHomeObservers() {
+    let obs = [];
 
-	obs.push(new Observer(HOME_VIDEO_CONFIG, onHorizontalVideoObserved));
-	obs.push(new Observer(HOME_VIDEO_CONFIG_V2, onHorizontalVideoObserved));
-	obs.push(new Observer(POST_CONFIG, onPostObserved));
-	obs.push(new Observer(VIDEO_IN_CONTAINER_CONFIG, onVideoObserved));
-	obs.push(new Observer(VIDEO_IN_SECTION_CONFIG, onVideoObserved));
-	obs.push(new Observer(HOME_MOVIE_CONFIG, onVideoObserved));
+    obs.push(new Observer(HOME_VIDEO_CONFIG, onHorizontalVideoObserved));
+    obs.push(new Observer(HOME_VIDEO_CONFIG_V2, onHorizontalVideoObserved));
+    obs.push(new Observer(POST_CONFIG, onPostObserved));
+    obs.push(new Observer(VIDEO_IN_CONTAINER_CONFIG, onVideoObserved));
+    obs.push(new Observer(VIDEO_IN_SECTION_CONFIG, onVideoObserved));
+    obs.push(new Observer(HOME_MOVIE_CONFIG, onVideoObserved));
 
-	return obs;
+    return obs;
 }
 
-function createTrendingObservers(){
-	let obs = [];
+function createTrendingObservers() {
+    let obs = [];
 
-	obs.push(new Observer(EXPANDED_VIDEO_CONFIG, onVerticalVideoObserved));
-	obs.push(new Observer(HORIZONTAL_VIDEO_CONFIG, onHorizontalVideoObserved));
-	obs.push(new Observer(VIDEO_CONTAINER_CONFIG, onVideoContainerObserved));
+    obs.push(new Observer(EXPANDED_VIDEO_CONFIG, onVerticalVideoObserved));
+    obs.push(new Observer(HORIZONTAL_VIDEO_CONFIG, onHorizontalVideoObserved));
+    obs.push(new Observer(VIDEO_CONTAINER_CONFIG, onVideoContainerObserved));
 
-	return obs;
+    return obs;
 }
 
-function createSearchObservers(){
-	let obs = [];
+function createSearchObservers() {
+    let obs = [];
 
-	obs.push(new Observer(SEARCH_VIDEO_CONFIG, onVerticalVideoObserved));
-	obs.push(new Observer(VERTICAL_VIDEO_CONFIG, onVerticalVideoObserved));
-	obs.push(new Observer(PLAYLIST_CONFIG, onPlaylistObserved));
-	obs.push(new Observer(CHANNEL_CONFIG, onChannelObserved));
-	obs.push(new Observer(SEARCH_MOVIE_CONFIG, onVideoObserved));
+    obs.push(new Observer(SEARCH_VIDEO_CONFIG, onVerticalVideoObserved));
+    obs.push(new Observer(VERTICAL_VIDEO_CONFIG, onVerticalSearchVideoObserved));
+    obs.push(new Observer(PLAYLIST_CONFIG, onPlaylistObserved));
+    obs.push(new Observer(CHANNEL_CONFIG, onChannelObserved));
+    obs.push(new Observer(SEARCH_MOVIE_CONFIG, onVideoObserved));
 
-	return obs;
+    return obs;
 }
 
-function createChannelHomeObservers(){
-	let obs = [];
+function createChannelHomeObservers() {
+    let obs = [];
 
-	return obs;
+    return obs;
 }
 
-function createChannelVideosObservers(){
-	let obs = [];
+function createChannelVideosObservers() {
+    let obs = [];
 
-	return obs;
+    return obs;
 }
 
-function createVideoObservers(){
-	let obs = [];
+function createVideoObservers() {
+    let obs = [];
 
-	obs.push(new Observer(COMMENT_CONFIG, onCommentObserved));
-	obs.push(new Observer(REPLY_COMMENT_CONFIG, onReplyCommentObserved));
+    obs.push(new Observer(COMMENT_CONFIG, onCommentObserved));
+    obs.push(new Observer(REPLY_COMMENT_CONFIG, onReplyCommentObserved));
 
-	obs.push(new Observer(NEXT_AUTOPLAY_CONFIG, onNextObserved));
-	obs.push(new Observer(NEXT_VIDEO_CONFIG, onNextObserved));
-	obs.push(new Observer(NEXT_PLAYLIST_CONFIG, onNextObserved));
-	obs.push(new Observer(NEXT_ITEM_SECTION_VIDEO_CONFIG, onNextObserved));
-	obs.push(new Observer(NEXT_ITEM_SECTION_PLAYLIST_CONFIG, onNextObserved));
+    obs.push(new Observer(NEXT_AUTOPLAY_CONFIG, onNextObserved));
+    obs.push(new Observer(NEXT_VIDEO_CONFIG, onNextObserved));
+    obs.push(new Observer(NEXT_PLAYLIST_CONFIG, onNextObserved));
+    obs.push(new Observer(NEXT_ITEM_SECTION_VIDEO_CONFIG, onNextObserved));
+    obs.push(new Observer(NEXT_ITEM_SECTION_PLAYLIST_CONFIG, onNextObserved));
 
-	obs.push(new Observer(VIDEOWALL_VIDEO_CONFIG, onVideowallVideoObserved));
+    obs.push(new Observer(VIDEOWALL_VIDEO_CONFIG, onVideowallVideoObserved));
 
-	obs.push(new Observer(MAIN_VIDEO_CONFIG, onMainVideoObserved));
+    obs.push(new Observer(MAIN_VIDEO_CONFIG, onMainVideoObserved));
 
-	obs.push(new Observer(VIDEO_MOVIE_CONFIG, onVideoObserved));
+    obs.push(new Observer(VIDEO_MOVIE_CONFIG, onVideoObserved));
 
-	return obs;
+    return obs;
 }
